db1a0e54b15d0874f9a5ebf67ae382525d8fc133
mokhs00 <mokhs00@naver.com>
2021-03-09 06:22:07 +0900

    add : data.sql

---
+++ data.sql
@@ -0,0 +1,365 @@
+INSERT INTO category (NAME) VALUES('에너지방향');
+INSERT INTO category (NAME) VALUES('인식기능');
+INSERT INTO category (NAME) VALUES('판단기능');
+INSERT INTO category (NAME) VALUES('생활양식');
+# ================================================================
+# ALTER TABLE category AUTO_INCREMENT = 1;
+# SET @COUNT = 0;
+# UPDATE category SET id = @COUNT:=@COUNT+1;
+
+
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('INFP','철벽 금사빠 토끼',
+'이성주의자이자 완벽주의자면서 엄청난 망상가에요. 온종일 망상에 빠져있기도 해요. 가끔은 우울한 생각에 빠져있기도 한데, 그런 내가 굉장히 고독해 보이고 멋지다고 생각해요. 연애에서도 혼자서 오만가지 생각을 하다가 금세 상대방에게 마음을 다 줘버리는 경우가 많아요. 멀티가 잘되지 않아 연락이 안 되는 경우가 있어 상대방이 답답해하기도 해요. 문제는 상대방이 답답해하는 거에 별로 신경 쓰지 않아서 종종 다툴 때가 있어요. 자기애가 강한 성격이라 상대방이 자기에 대해 안 좋게 생각하는 것 같다고 느끼면, 생각이 많아지고, 그러다 보면 마음이 확 식어버리게 돼요. 나에게 끊임없이 믿음을 주려 노력하고, 나에게 열정적인 사람을 만나면 행복지수가 무한히 상승해요!',
+'/img/INFP.png');
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('INFJ','낯가림 쩌는 짝사랑꾼 기린',
+'생각이 너무 많고 낯가림마저 심해서 혼자 짝사랑만 하다 인연을 놓치는 경우가 많아요. 썸 탈 때도 상대방 표정이 조금이라도 안 좋아지면 머릿속으로 오만가지 생각을 다하다 혼자 결론지어버려 상대방이 당황하는 경우가 많았어요. 하지만 이왕 한 번 마음을 먹고 연애를 시작한다면 상대방에게 한없이 잘해주는 성격이에요. 마음이 맞는 상대라면 정말 한없이 행복하게 지낼 수 있답니다. 나와 마음이 잘 맞고, 나에게 먼저 스스럼없이 다가와 주는 사람을 만나면 정말 행복한 연애를 할 수 있어요!',
+'/img/INFJ.png');
+
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('INTJ','전략적인 사랑꾼 호랑이',
+'개인주의자이자 원리원칙을 철저하게 지켜야 한다는 생각이 정신 깊숙하게 박힌 사람이에요. 하루를 초 단위까지 계산해서 계획을 세울 정도로 철두철미하고, 감정에 휘둘리는 것을 싫어하는, 말 그대로 완벽주의 성격이에요. 썸 탈 때 내가 하는 모든 행동들, 내뱉는 단어들은 모두 다 전략적으로 머릿속에서 수없이 시뮬레이션해 본 것들이에요. 그러다 보니 사람에게 정을 붙이는 데 오랜 시간이 걸려 연인과의 관계에 있어 뜨거워지는 데 다소 시간이 필요해요. 내가 전략을 수립할 틈도 주지 않고 먼저 다가와 주고, 칭찬을 많이 해주는 사람이라면 얼마 지나지 않아 그 사람에게 푹 빠져 연인 사이가 될 거예요.',
+'/img/INTJ.png');
+
+
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('INTP','영리한 사랑꾼 부엉이',
+'매사 진지하고, 주관이 뚜렷한 성격의 소유자로 인생은 오직 마이웨이라고 생각하는 유형이에요. 감정 기복이 심하지 않고, 감정 표현도 잘 하지 않아 상대방이 접근하기 조금 어려워한답니다. 더불어 인간관계를 매우 계산적으로 접근하고, 싫어하는 사람이 명확하기 때문에 마음에 드는 사람을 딱 만나기가 힘들어 연애를 시작하기까지 굉장히 오랜 시간이 걸리기도 해요. 건설적인 대화를 좋아하고, 부지런해서 함께 무얼 하는 걸 주도적으로 이끌어주는 사람을 만난다면 엄청난 사랑꾼으로 변할 거예요!',
+'/img/INTP.png');
+
+
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('ISTJ','진득한 사랑꾼 하마',
+'계획적이고, 독립심이 강하며 뭐든 차근차근 오랫동안 진행하는 성격이에요. 약속 시간을 어기는 사람을 좋아하지 않고, 독립심이 강해 사람들 만나는 걸 그리 즐기지 않아요. 하지만 책임감이 강해서 절대 바람을 피우지 않고, 연애의 시간이 길어질수록 상대방에게 더 잘하는 모습을 보여준답니다. 이 분과 연애를 하면 바람에 대한 걱정은 안 해도 돼요. 애당초 연인이 없어도 이성에게 틈을 주지 않는 성격이기 때문이에요. 나와 만날 때 내 믿음을 배신하지 않는 사람, 오래도록 나와 함께해 줄 수 있는 사람을 만나면 상대방에게 헌신하는 모습을 볼 수 있을 거예요.',
+'/img/ISTJ.png');
+
+
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('ISFJ','연인에게 충성하는 사슴',
+'끈기 있고, 성실하며, 안정감이 있는 연애를 추구하는 유형이에요. 소심하고, 타인의 감정에 민감한 성격이라 연인과의 관계에 있어서 항상 말을 예쁘게 하려고 하고 관계에 있어 조심하는 부분들이 있답니다. 이러한 성격 때문인지 좋은 신랑/신부감이라는 소리를 듣기도 하고, 실제로 어른들도 엄청 좋아하는 경향이 있답니다. 나는 나에 대해서 상대방에게 잘 어필하지 못하기 때문에, 내 매력을 먼저 눈치채고 다가와 주는 사람을 만나는 게 좋아요.',
+'/img/ISFJ.png');
+
+
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('ISTP','스마트&프리한 연애 고양이',
+'누가 시키지 않아도 혼자서 뭐든 척척 잘 해나가는 스타일이에요. 연애도 마찬가지예요. 알아서 척척 잘 해낸답니다. 하지만 자유로운 연애를 추구하기 때문에 자기 이야기를 잘 안 하기도 하고, 혼자만의 시간을 즐기는 경우도 많아요. 심한 경우 가끔 잠수를 타버려서 상대방이 답답해하는 경우가 있어요. 그래도 똑똑해서 사소한 변화를 잘 눈치채고, 위하려는 노력을 많이 해서 상대방이 금세 섭섭한 마음을 털어내고 감동하게 하는 경우들이 종종 있어요. 나를 믿어주고, 개인 시간을 존중해 주는 사람과 만나면 세상 행복하답니다.',
+'/img/ISTP.png');
+
+
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('ISFP','온화한 사랑꾼 양',
+'만사가 귀찮고, 집에 있는 걸 엄청 좋아하는 완전한 집돌, 집순이에요. 너무 모든 일들을 귀찮아하다 보니 연애할 때 연락도 두절되어버려요. 그래서 연인이 힘들어하는 경우가 있어요. 하지만 눈치가 빨라서 이러한 부분이 다툼으로 이어지기 전에 상대방의 감정 변화를 눈치채고 갈등과 불화를 해결하고자 노력한답니다. 이는 갈등과 다툼을 싫어해서 노력하는 거지 마음속에 꾹꾹 눌러 담고 있기 때문에 언제 폭발할지 나도 몰라요. 자존감이 높고, 다정다감해서 애정표현을 잘하는 상대방을 만나면, 연애가 엄청 행복해요!',
+'/img/ISFP.png');
+
+
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('ENFJ','연인만 바라보는 핵인싸 강아지',
+'활발하면서 믿음직스럽고, 멘탈이 강해 마치 강아지 같은 모습을 보여줘요. 아무리 주인이 뭐라 해도 주인만 바라보는 강아지 말이에요! 다만 워커홀릭 기질이 있어서 일과 커리어 발전에도 욕심이 많아요. 가끔은 만나기 힘들 때도 있지만, 또 막상 만나면 오직 상대방만을 위한 모습들을 한없이 보여줘요. 센스도 넘쳐서 상대방은 자신이 사랑받는다는 느낌을 확실하게 받을 수 있답니다. 나의 계획성, 커리어에 대한 열정을 존중해 주고, 응원하려 노력해 주는 사람과 만나면 세상 제일 행복한 연애가 될 거예요.',
+'/img/ENFJ.png');
+
+
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('ENTP','마이웨이 사랑꾼 앵무새',
+'세상엔 오직 나뿐, 내가 하는 게 뭐든 정답이다 생각하고 강하게 밀어붙이는 유형이에요. 비단 일뿐만이 아니에요. 일도 연애도 내가 생각한 대로 흘러가지 않으면 답답하고 짜증 나요. 그래서 맨날 의견 주장만 하다가 혼나기도 하고, 결국 상대방과의 의견 차이를 좁히지 못해서 심하게 싸우게 되는 경우도 있어요. 그렇지만 그게 진짜 내면의 모습은 아니에요. 겉으로 보이는 모습과는 다르게 매우 여린 마음을 가지고 있어서 쉽게 상처받기도 해요. 그런 나를 잘 보듬어주고, 때때로 직설적이게 표현하는 나를 잘 받아주는 사람과 연애하면 좋아요.',
+'/img/ENTP.png');
+
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('ENFP','화끈 똑똑 사랑꾼 코끼리',
+'하고 싶은 것도 많고, 실제로 하는 것도 엄청 많아요! 근데 감정 기복이 심해서 그런지 끝을 보는 경우가 없어요. 친구들이랑 노는 걸 좋아하고, 친구들도 나랑 노는 걸 좋아해서 약속도 끊이지 않아요. 그래서 연인이 서운해할 만한 일들이 많아요. 눈치가 빨라서 알아도 모른척하지만, 그게 또 얼굴에 다 드러나서 결국은 갈등을 겪고 말아요. 그래도 거짓말은 절대 하지 않아서 대화로 잘 풀면 금방 관계가 회복돼요. 친구들과 만나는 것들을 조금은 이해해 주고, 함께 같이 놀 수 있는 사람이 연인이라면 그 누구보다 좋은 관계가 될 수 있어요!',
+'/img/ENFP.png');
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('ENTJ','야심찬 연애 불도저 사자',
+'똑똑하고, 논리적이고, 추진력이 좋아 화끈하게 밀어붙이는 불도저 같은 유형이에요. 내가 잘났다는 생각에 가끔 상대방을 무시하는 행동을 보이기도 해요. 맞아요. 진짜 잘났는데 그러면 안 된다는 것도 알아요. 그래서 연인과 다투기도 하지만, 근본적으로는 상대방을 무시하려는 의도도 아니고 남에게 피해 주는 걸 싫어하는 성격이어서 금방 화해해요. 갈등을 무조건 해결해야 직성이 풀리기 때문에 다툼을 오래 끌지도 않아요. 그래서 나와 비슷하게 자기 일 잘하는 사람을 만나거나 나를 더 많이 좋아해 주는 사람과 만나면 행복한 연애가 될 거예요.',
+'/img/ENTJ.png');
+
+
+
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('ESTP','애정표현 끝판왕 얼룩말',
+'활발하며, 자신감 넘치는 태도, 타고난 손재주로 애정표현의 끝을 보여주는 유형이에요. 연애를 하면 적극적으로 표현하는 스타일이라 바뀐 모습에 상대가 당황하기도 해요. 하지만 그 모습이 너무 매력적이라 상대가 당신에게 더욱 호감을 느끼게 되고, 당신을 더 좋아하게 될 거예요. 가끔 약간 허세기 있는 모습과 때로는 상대방을 잘 이해하려 하지 않는 모습을 보여 종종 갈등을 겪는 문제가 있을 수 있어요. 나를 존중해 주고, 함께 표현을 많이 해주는 사람을 만나면 행복한 연애가 될 거예요!',
+'/img/ESTP.png');
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('ESFP','러브 에너자이저 젖소',
+'낄 데 다 끼고, 안 낄 곳도 다 껴야되는 성격이라 그런지 연애에 있어서도 에너지가 넘치는 유형이에요. 넘치는 에너지 때문인지 이상하게 나는 모든 집단에 속해 있기때문에 정신이 없기도 해요. 그래서 그런지 누군가를 만나게 될 확률이 가장 높아요. 만약 나와 썸을 오전에 타기 시작했으면 오후에는 1일이 되어 있어야 해요. 연인에겐 조금 미안하지만 연애를 하면 데이트 코스는 내가 하고 싶은 거, 먹고 싶은 거 위주로 짜야 해요. 사교성이 너무 좋아서 때로는 연인이 질투하기도 해요. 나의 사교성을 존중해 주고 배려해 주는 사람과 연애를 하면 최고예요!',
+'/img/ESFP.png');
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('ESTJ','연인만 바라보는 곰',
+'꼼꼼함, 계획적, 고집 셈 성격으로 밀당 같은 건 전혀 할 줄도, 할 생각도 없는 유형이에요. 하지만 밀당을 하지 않는다는 게 상대방을 위해 최선을 다한다는 의미는 아니에요. 그냥 자신에 대한 관심이 많아 상대방에게까지 관심을 쏟지 않는 거예요. 그런 모습이 연인의 입장에서는 잘 보이지 않아서 연인 입장에서는 조금 섭섭해하고, 서운해할 수는 있어요. 다만, 연애의 감정이 깊어지면 깊어질수록 오로지 그 사람만을 위해서 최선을 다하는 순애보 같은 모습을 보여줘요. 나와의 약속을 잘 지켜주고, 의견을 확실하게 말해주는 사람이 나에게 가장 잘 맞는 사람이에요!',
+'/img/ESTJ.png');
+
+INSERT INTO result (VALUE, title, TEXT, img_path)
+VALUES('ESFJ','연애눈치 만랩 말',
+'철두철미하고, 적극적인 성격인데 눈치마저 빨라요. 가끔은 소심하다는 얘기를 듣기도 하지만, 내 연인을 위한 일들에서는 한없이 적극적인 모습을 보여요. 이야기 들어주고, 공감을 정말 잘해줘서 연인에게 있어 너무나도 든든한 존재랍니다. 그렇지만 상대방의 감정에 나도 모르게 동화되어버려서 연인이 우울해하면 나도 덩달아 우울해져버려요. 그래서 밝고 긍정적이면서 먼저 다가와 주고, 함께 이야기를 해 주는 사람이면 더없이 좋은 연인 관계가 될 거예요.',
+'/img/ESFJ.png');
+
+
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ENFJ") AS t) WHERE value = "INFP";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ISFP") AS t)WHERE value = "INFP";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ENTP") AS t)WHERE value = "INFJ";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ISTP") AS t)WHERE value = "INFJ";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ENTP") AS t)WHERE value = "INTJ";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ISFJ") AS t)WHERE value = "INTJ";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ENTJ") AS t)WHERE value = "INTP";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ISTJ") AS t)WHERE value = "INTP";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ESTP") AS t)WHERE value = "ISTJ";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "INFP") AS t)WHERE value = "ISTJ";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ESFP") AS t)WHERE value = "ISFJ";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ENFP") AS t)WHERE value = "ISFJ";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ESTJ") AS t)WHERE value = "ISTP";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "INFP") AS t)WHERE value = "ISTP";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ESFJ") AS t)WHERE value = "ISFP";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ENFP") AS t)WHERE value = "ISFP";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "INFP") AS t)WHERE value = "ENFJ";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ESTP") AS t)WHERE value = "ENFJ";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "INTJ") AS t)WHERE value = "ENTP";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ESTJ") AS t)WHERE value = "ENTP";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "INTJ") AS t)WHERE value = "ENFP";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ESFP") AS t)WHERE value = "ENFP";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "INTP") AS t)WHERE value = "ENTJ";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ESFJ") AS t)WHERE value = "ENTJ";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ISTJ") AS t)WHERE value = "ESTP";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "INFJ") AS t)WHERE value = "ESTP";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ISFJ") AS t)WHERE value = "ESFP";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "INFJ") AS t)WHERE value = "ESFP";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ISFP") AS t)WHERE value = "ESTJ";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ENFJ") AS t)WHERE value = "ESTJ";
+UPDATE result SET positive_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "ISTP") AS t)WHERE value = "ESFJ";
+UPDATE result SET negative_id = (SELECT id FROM (SELECT * FROM result r WHERE value = "INFJ") AS t)WHERE value = "ESFJ";
+
+# ================================================================
+
+INSERT INTO question (category_id, TEXT)
+VALUES(4, '연인과 놀이동산을 간 상황');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(1, '어떤 놀이기구를 탈지 미리 계획을 세운다', 'J');
+INSERT INTO choice (question_id, text, value)
+VALUES(1, '일단 재밌어 보이는 것부터 탄다', 'P');
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(2, '소개팅을 나간 당신, 서로 첫 만남에 어색하다');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(2, '준비해온 멘트들을 건네며 대화를 시작한다', 'S');
+INSERT INTO choice (question_id, text, value)
+VALUES(2, '미소를 지으며 상대가 말할 때까지 기다린다', 'N');
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(3, '연인의 생일 선물을 준비할 때');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(3, '계획이 다 있지! 미리 생각해서 준비한다', 'T');
+INSERT INTO choice (question_id, text, value)
+VALUES(3, '헉! 어떡하지,, 생일 직전에 급하게 준비한다', 'F');
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(1, '연인이 길을 가다가 누군가와 부딪혔다');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(4, '저기요! 사과하셔야죠! 일단 따지고 본다', 'E');
+INSERT INTO choice (question_id, text, value)
+VALUES(4, '괜찮아? 다친데 없어? 연인을 살핀다.', 'I');
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(2, '누군가 나를 짜증 나게 했을 때 연인과 통화하면');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(5, '오늘 나에게 일어난 일을 처음부터 끝까지 다 이야기한다', 'S');
+INSERT INTO choice (question_id, text, value)
+VALUES(5, '일단 그 사람 욕부터 하고, 있었던 일은 대충 얘기한다', 'N');
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(3, '연인과의 말다툼 중이다');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(6, '연인의 기분이 조금 상하더라도 팩트가 우선', 'T');
+INSERT INTO choice (question_id, text, value)
+VALUES(6, '팩트보다는 연인의 기분이 우선', 'F');
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(1, '내가 선호하는 데이트');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(7, '핫플에서 사진도 찍고 맛집 탐방하기', 'E');
+INSERT INTO choice (question_id, text, value)
+VALUES(7, '집에서 편하게 TV 보며 꽁냥거리기', 'I');
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(4, '연인과 데이트를 할 때 나는 주로');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(8, '미리 예약하고 어디 갈지 데이트 코스를 짠다', 'J');
+INSERT INTO choice (question_id, text, value)
+VALUES(8, '만나서 상황따라 당기는 것, 가고 싶은 곳을 간다', 'P');
+
+
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(3, '연인이 나의 생일에 선물을 준다면');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(9, '먼저 물어보고 내가 필요한 걸 줬으면 좋겠다', 'T');
+INSERT INTO choice (question_id, text, value)
+VALUES(9, '마음이 느껴지기만 하면 어떤 선물이라도 좋다', 'F');
+
+
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(1, '나는 하루 종일 집에 있으면');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(10, '아~~ 심심해~ 어디라도 나가야 해!', 'E');
+INSERT INTO choice (question_id, text, value)
+VALUES(10, '너무 좋아,, 집이 최고!', 'I');
+
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(1, '오랜만에 소개팅에 나갔는데 이상형을 만났다');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(11, '상대방이 어떤 사람인지 알고 싶어 폭풍 질문을 던진다', 'E');
+INSERT INTO choice (question_id, text, value)
+VALUES(11, '나에 관한 좋은 이야기들을 은근 많이 하면서 나를 어필한다', 'I');
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(4, '연인이 힘든 상황에 처했다');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(12, '상황을 해결하기 위해 현실적인 해결책을 제시한다', 'J');
+INSERT INTO choice (question_id, text, value)
+VALUES(12, '연인의 감정에 공감하고 정신적인 지지를 해준다', 'P');
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(1, '연인과 길을 걷다 연인의 동창을 만났다');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(13, '마치 알던 사이인 것처럼 같이 이야기한다', 'E');
+INSERT INTO choice (question_id, text, value)
+VALUES(13, '난 투명 인간.. 대화가 끝나길 기다린다', 'I');
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(4, '썸타는 사람이 3시간 넘게 연락이 없다');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(14, '신경쓰여,, 수시로 핸드폰을 들여다본다', 'J');
+INSERT INTO choice (question_id, text, value)
+VALUES(14, '일이 바쁜가? 별로 신경 안 쓴다', 'P');
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(2, '애인 : 나 뭐 변한 거 없어?');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(15, '음,, 어디보자,, ', 'S');
+INSERT INTO choice (question_id, text, value)
+VALUES(15, '머리 잘랐어? 단 번에 알아 차린다.', 'N');
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(4, '연인과 함께 게임중');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(16, '연인사이라도 지는건 못 참지! 승부욕 발동', 'J');
+INSERT INTO choice (question_id, text, value)
+VALUES(16, '연인사이에 이기고 지는게 뭐가 중요해. 재밌으면 만족', 'P');
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(2, '연인에게서 "OO야 뭐해?" 카톡이 왔다');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(17, '톡을 읽지 않고 남겨두는 건 참을 수 없어! 바로 답한다', 'S');
+INSERT INTO choice (question_id, text, value)
+VALUES(17, '톡이 온 걸 알아도 내 볼일이 우선이야! 나중에 답한', 'N');
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(3, '연인과의 기념일 날, 회사에 일이 생겼다');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(18, '연인과의 약속을 취소하고 야근', 'T');
+INSERT INTO choice (question_id, text, value)
+VALUES(18, '야근이고 뭐고 오늘은 연인을 만나러 간다', 'F');
+
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(2, '연인과 밥 먹으러 간다. 어디로 갈까?');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(19, '검증된 맛집이 최고! 단골집 고고!', 'S');
+INSERT INTO choice (question_id, text, value)
+VALUES(19, '저기 새로운 음식점 발견! 새 맛집 도전!', 'N');
+
+
+
+INSERT INTO question (category_id, TEXT)
+VALUES(3, '연인이 기념일을 까먹어서 화가 났다');
+
+INSERT INTO choice (question_id, text, value)
+VALUES(20, '논리적으로 따지며 화를 낸다.', 'T');
+INSERT INTO choice (question_id, text, value)
+VALUES(20, '어떻게 이럴 수가 있어? 날 사랑하긴 해?라며 화를 낸다.', 'F');
+
+#===================================================================================
